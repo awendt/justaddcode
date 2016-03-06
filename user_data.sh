318b27324fa99c7e90585e70a70176fab0f04fbe
André Wendt <awendt@freeshell.org>
2016-03-06 17:53:24 +0100

    Install dokku-letsencrypt by default

--- user_data.sh
+++ user_data.sh
@@ -5,3 +5,5 @@ chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
+
+dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git
