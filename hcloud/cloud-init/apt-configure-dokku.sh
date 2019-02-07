118290d03ea00bde603022177dabcca77b065b1b
André Wendt <awendt@freeshell.org>
2019-02-07 08:56:07 +0100

    Working installation

--- hcloud/cloud-init/apt-configure-dokku.sh
+++ hcloud/cloud-init/apt-configure-dokku.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 curl -L https://packagecloud.io/dokku/dokku/gpgkey | sudo apt-key add -
-echo "deb https://packagecloud.io/dokku/dokku/ubuntu/ trusty main" > /etc/apt/sources.list.d/dokku_dokku.list
+echo "deb https://packagecloud.io/dokku/dokku/ubuntu/ bionic main" > /etc/apt/sources.list.d/dokku_dokku.list
