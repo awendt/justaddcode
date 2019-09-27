ac32b6e4c29bed3f7bd750cbc2b1ad86075c148d
André Wendt <awendt@freeshell.org>
2019-09-27 08:47:09 +0200

    terraform 0.12upgrade

--- shared.tf
+++ shared.tf
@@ -4,12 +4,15 @@
 
 # Set the variable value in *.tfvars file
 # or using -var="hcloud_token=..." CLI option
-variable "hcloud_token" {}
-variable "hostname" {}
+variable "hcloud_token" {
+}
+
+variable "hostname" {
+}
 
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
-  token = "${var.hcloud_token}"
+  token = var.hcloud_token
 
   version = "~> 1.13.0"
 }
@@ -20,6 +23,6 @@ provider "template" {
 
 # Create a new SSH key
 resource "hcloud_ssh_key" "default" {
-  name = "boskoop"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  name       = "boskoop"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
--- v1.tf
+++ v1.tf
@@ -1,8 +1,8 @@
 # Render a part using a `template_file`
 data "template_file" "script" {
-  template = "${file("./cloud-config.yml")}"
+  template = file("./cloud-config.yml")
 
-  vars {
+  vars = {
     fqdn = "v1.${var.hostname}"
   }
 }
@@ -17,31 +17,30 @@ data "template_cloudinit_config" "config" {
   part {
     filename     = "init.cfg"
     content_type = "text/cloud-config"
-    content      = "${data.template_file.script.rendered}"
+    content      = data.template_file.script.rendered
   }
 
   part {
     content_type = "text/x-shellscript"
-    content      = "${file("./cloud-init/dokku-config.sh")}"
+    content      = file("./cloud-init/dokku-config.sh")
   }
 
   part {
     content_type = "text/x-shellscript"
-    content      = "${file("./cloud-init/secrets.sh")}"
+    content      = file("./cloud-init/secrets.sh")
   }
-
 }
 
 # Create a new server running debian
 resource "hcloud_server" "v1_justaddco_de" {
-  location = "fsn1"
-  name = "v1.${var.hostname}"
-  image = "ubuntu-18.04"
+  location    = "fsn1"
+  name        = "v1.${var.hostname}"
+  image       = "ubuntu-18.04"
   server_type = "cx11"
-  ssh_keys = ["${hcloud_ssh_key.default.id}"]
-  user_data = "${data.template_cloudinit_config.config.rendered}"
+  ssh_keys    = [hcloud_ssh_key.default.id]
+  user_data   = data.template_cloudinit_config.config.rendered
 }
 
-output "v1 IP address" {
-  value = "${hcloud_server.v1_justaddco_de.ipv4_address}"
+output "v1_ip_address" {
+  value = hcloud_server.v1_justaddco_de.ipv4_address
 }
